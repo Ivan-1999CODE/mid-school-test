b9b2a35f193ec56027c3db5a86aa7affcdf2f0df
Ivan <chenchong885@yahoo.com.tw>
2026-04-27 01:55:51 +0800

    Update vite config to support Cloudflare deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mid-school-test/',
+  // base: '/mid-school-test/', // 註解掉此行，讓預設的 root ('/') 適用於 Cloudflare Pages
 })
